706f88601a37479798048d9322f282f3e8cb094e
Filip Kozłowski <filip.kozlowski.1@student.put.poznan.pl>
2024-07-20 17:17:41 +0200

    POST test

--- client/src/App.js
+++ client/src/App.js
@@ -2,23 +2,35 @@
 
 import axios from 'axios';
 import './App.css';
+import React, { useEffect, useState } from 'react';
 
 //data will be the string we send from our server
-const apiCall = () => {
-  axios.get('http://localhost:8080').then((data) => {
+const apiCall2 = () => {
+  axios.get('http://localhost:8080/api/list').then((data) => {
     //this console.log will be in our frontend console
     console.log(data)
   })
 }
 
+const apiCall = () => {
+    axios.post('http://localhost:8080/api/products', { id: 1 })
+        .then(response => {
+            console.log(response.data)
+        })
+        .catch(err => {
+            console.error(err);
+        });
+}
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+        <header className="App-header">
 
-        <button onClick={apiCall}>Make API Call</button>
+            <button onClick={apiCall}>Console log full data</button>
+            <button onClick={apiCall2}>Log message</button>
 
-      </header>
+        </header>
     </div>
   );
 }
--- server/index.js
+++ server/index.js
@@ -1,8 +1,15 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const sqlite3 = require('sqlite3').verbose();
 
+// Middleware
 app.use(cors())
+app.use(express.json());
+
+// Initialize SQLite database
+const db = new sqlite3.Database('./data/database.db');
+
 
 app.get('/', (req, res) => {
       res.send('Hello from our server!')
@@ -11,3 +18,26 @@ app.get('/', (req, res) => {
 app.listen(8080, () => {
       console.log('server listening on port 8080')
 })
+
+app.get('/api/list', (req, res) => {
+      res.send("Test");
+})
+
+app.post('/api/products', (req, res) => {
+      const productId = req.body.id; // Get the product ID from request body
+
+      if (!productId) {
+            return res.status(400).json({ error: 'Product ID is required' });
+      }
+
+      db.get('SELECT * FROM products WHERE id = ?', [productId], (err, row) => {
+            if (err) {
+                  console.error(err);
+                  res.status(500).json({ error: 'Database query failed' });
+            } else if (!row) {
+                  res.status(404).json({ error: 'Product not found' });
+            } else {
+                  res.json(row);
+            }
+      });
+});
