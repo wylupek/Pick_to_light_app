10c54fe1e78f4510b47e556512935f8f85c2a926
wylupek <filip@tapoczta.pl>
2024-08-03 13:27:15 +0200

    Add _variables.scss

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import './App.css';
+import './App.scss';
 import React, { useEffect, useState } from 'react';
 import SuppliersTable from './components/SuppliersTable/SuppliersTable';
 import ProductsTable from './components/ProductsTable/ProductsTable';
